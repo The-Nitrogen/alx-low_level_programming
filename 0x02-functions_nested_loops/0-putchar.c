fddc46cb8b2dddd8947b8b3ed947f1a84642fa2c
The-Nitrogen <choaibderiham0000@gmail.com>
2023-09-12 15:42:55 -0700

    hello world

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,11 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - Prints Holberton as a message.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
+printf("_putchar\n");
+return (0);
 }
